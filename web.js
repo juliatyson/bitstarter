10e36f7d3707dde3156c02a3aadd503555698048
juliatyson <jht@princeton.edu>
2013-08-11 23:41:53 +0000

    readded web.js

--- web.js
+++ web.js
@@ -3,9 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var read = new Buffer(200);
-    read = fs.readFileSync(index.html);
-    response.send(read.toString());
+    
+    response.send(fs.readFileSync(index.html).toString("utf-8", 0, 27));
 });
 
 var port = process.env.PORT || 5000;
