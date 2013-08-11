3cf9319aac6c3edfa12a61f0238bc23b193ffe2e
juliatyson <jht@princeton.edu>
2013-08-11 23:26:07 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-var read = new Buffer(200);
-read = fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
-  response.send(read.toString());
+    var read = new Buffer(200);
+    read = fs.readFileSync(index.html);
+    response.send(read.toString());
 });
 
 var port = process.env.PORT || 5000;
