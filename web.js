2ccdfa7d137755cfb987c7d85031eafc514ecda7
juliatyson <jht@princeton.edu>
2013-08-12 00:06:49 +0000

    updated web.js 5

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 
 app.get('/', function(request, response) {
-    fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text) {
+    fs.readFile('/bitstarter/index.html', 'utf8', function(err, text) {
 
     response.send(text);
    });
