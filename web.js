4d280227863a8e33fb3a5be6f21e2ed22b41a872
juliatyson <jht@princeton.edu>
2013-08-23 01:54:13 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,17 +1,16 @@
 var express = require('express'), fs = require('fs');
 var file = "index.html";
 
-var app = express.createServer(express.logger());
-
+var app = express(express.logger()); 
 
 app.get('/', function(request, response) {
-    var readfile = fs.readFileSync(file).toString(); 
-    response.send(readfile);
-  
+   var readfile = fs.readFileSync(file).toString(); 
+   response.send(readfile);
+ 
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
+	console.log("Listening on " + port);
 
-  console.log("Listening on " + port);
 });
